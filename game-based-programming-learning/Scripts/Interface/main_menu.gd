274c9be23b4d8d7316841225c72c76e0c3b19844
Idehen99 <idehen.idahor@mni.thm.de>
2026-01-26 00:38:05 +0100

    startPoint
    added basic main menu

---
+++ game-based-programming-learning/Scripts/Interface/main_menu.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	pass
+	
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Interface/save_game.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
