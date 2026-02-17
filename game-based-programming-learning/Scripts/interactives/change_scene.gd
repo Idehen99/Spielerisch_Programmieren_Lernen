dbd757f31c54a5cc72ed8c03c2e2a346afcd17b4
Idehen99 <idehen.idahor@mni.thm.de>
2026-02-17 04:24:50 +0100

    solved bug

--- game-based-programming-learning/Scripts/interactives/change_scene.gd
+++ game-based-programming-learning/Scripts/interactives/change_scene.gd
@@ -7,4 +7,6 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		Settings._on_change_scene_spawn_position(new_position_for_player)
-		get_tree().change_scene_to_file(destination)
+		get_tree().call_deferred("change_scene_to_file", destination)
+		
+		
