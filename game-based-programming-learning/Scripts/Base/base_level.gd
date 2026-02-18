49d034a820d17c9fca68493af4ff729df4869760
Idehen99 <idehen.idahor@mni.thm.de>
2026-02-18 02:47:54 +0100

    added first block riddle to forrest level
    added first block riddle to forrest level so now you need to solve the riddle to the tree to get back your movement

--- game-based-programming-learning/Scripts/Base/base_level.gd
+++ game-based-programming-learning/Scripts/Base/base_level.gd
@@ -1,9 +1,12 @@
 extends Node2D
 @onready var play = preload("res://Scenes/character/player/player_selfmade.tscn") #preload("res://Scenes/Test/Test_Player.tscn")
+var player
 func _ready() -> void:
+	if get_tree().get_first_node_in_group("codelevel"):
+		play = preload("res://Scenes/character/player/code_player.tscn")
 	if get_tree().get_first_node_in_group("player")==null:
 		print("yes")
-		var player = play.instantiate()
+		player = play.instantiate()
 		add_child(player)
 		print(player.get_groups())
 		player.add_to_group("player")
@@ -11,3 +14,18 @@ func _ready() -> void:
 	Settings.current_scene = scene_file_path
 	
 	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == player:
+		$Area2D/Timer.start(5)
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body == player:
+		$Area2D/Timer.stop()
+
+
+func _on_timer_timeout() -> void:
+	if player.has_method("_free_him"):
+		player._free_him()
--- game-based-programming-learning/Scripts/inLevelManagement/code_control.gd
+++ game-based-programming-learning/Scripts/inLevelManagement/code_control.gd
@@ -6,10 +6,11 @@ class_name CodeControl
 @onready var builder = $ProgrammBuilder
 @onready var editor = $CodeEditor
 @onready var posi 
+var player
 var play
 func _ready():
 	if get_tree().get_first_node_in_group("player")!=null:
-		var player = get_tree().get_first_node_in_group("player")
+		player = get_tree().get_first_node_in_group("player")
 		editor.visible = true
 		runner.player = player
 		posi = player.global_position
@@ -17,6 +18,7 @@ func _ready():
 
 
 func _on_play_pressed() -> void:
+	posi = player.global_position
 	$play.disabled = true
 	var program_data = builder.build_program(editor.code_container)
 	runner.run_block_list(program_data)
